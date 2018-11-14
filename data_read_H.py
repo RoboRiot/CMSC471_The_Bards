be7081149a2d347e5b1728f7ba12f305b9a8ad75
Harrison Mann <mannhar1@umbc.edu>
2018-11-14 18:13:38 -0500

    adding data_read_H

---
+++ data_read_H.py
@@ -0,0 +1,46 @@
+#Reading in data from data set
+#userID's and movieID's will be moved into a dictionary matching them to indices which will be used for the matrix
+
+import os
+
+filepath = "C:\\Users\\Harrison\\Documents\\CMSC_471_Project_Code\\ml-latest\\ml-latest\\movies.csv"
+
+file = open(filepath, "rt", encoding="utf8")
+
+movie_ids = []
+movie_titles = []
+
+i = 0
+for line in file:
+	if (i == 0):
+		#will be skipping the first line since it just holds the column labels
+		i = i+1
+	else:
+		movie_ids.append(line.split(',')[0])
+		movie_titles.append(line.split(',')[1])
+		
+		
+#print (movie_ids[34])
+#print (movie_ids[112])
+#print (len(movie_ids))
+
+
+filepath2 = "C:\\Users\\Harrison\\Documents\\CMSC_471_Project_Code\\ml-latest\\ml-latest\\ratings.csv"
+
+file2 = open(filepath, "rt", encoding="utf8")
+
+user_ids = []
+
+i=0
+for line in file2:
+	if (i == 0):
+		#will be skipping the first line since it just holds the column labels
+		i=i+1
+	elif line.split(',')[0] not in user_ids:
+		user_ids.append(line.split(',')[0])
+		
+#print (user_ids[0])
+#print (user_ids[1])
+#print (user_ids[4])
+
+
