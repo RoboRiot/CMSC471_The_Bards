b49f1653ae038c0bae2810c063e240007f6eb94f
Harrison Mann <mannhar1@umbc.edu>
2018-11-18 15:21:03 -0500

    pushing updated data_read_H.py using small dataset

--- data_read_H.py
+++ data_read_H.py
@@ -4,9 +4,7 @@
 import os
 import numpy
 
-filepath = "C:\\Users\\Harrison Mann\\Documents\\CMSC_471_Project_Code\\ml-latest\\movies.csv"
-
-
+filepath = "C:\\Users\\Harrison Mann\\Documents\\CMSC_471_Project_Code\\ml-latest-small\\movies.csv"
 
 file = open(filepath, "rt", encoding="utf8")
 
@@ -21,16 +19,11 @@ for line in file:
 	else:
 		movie_ids.append(line.split(',')[0])
 		movie_titles.append(line.split(',')[1])
-		
-		
-#print (movie_ids[34])
-#print (movie_ids[112])
-#print (len(movie_ids))
 
 
-filepath2 = "C:\\Users\\Harrison Mann\\Documents\\CMSC_471_Project_Code\\ml-latest\\ratings.csv"
+filepath2 = "C:\\Users\\Harrison Mann\\Documents\\CMSC_471_Project_Code\\ml-latest-small\\ratings.csv"
 
-file2 = open(filepath, "rt", encoding="utf8")
+file2 = open(filepath2, "rt", encoding="utf8")
 
 user_ids = []
 
@@ -41,16 +34,39 @@ for line in file2:
 		i=i+1
 	elif line.split(',')[0] not in user_ids:
 		user_ids.append(line.split(',')[0])
-		
-#print (user_ids[0])
-#print (user_ids[1])
-#print (user_ids[4])
 
 
 rows = len(user_ids)
 cols = len(movie_ids)
 
-a = numpy.zeros(shape=(5,2))
-print (a)
 
+a = numpy.ones(shape=(rows,cols))
 
+a = numpy.multiply(a, -1)
+
+print ("here?")
+
+i=0
+for line in file2:
+	print("here?")
+	if(i == 0):
+		#will be skipping the first line since it just holds the column labels
+		i=i+1
+	#elif (user_ids.index(line.split(',')[0]) < 100): THIS LINE WAS FOR WHEN WE WERE USING THE LARGE SET AND LIMITING THE NUMBER OF USERS
+	else:
+		linesplit = line.split(',')
+		
+		#first we find the index of the user id in the user_ids array
+		curr_user = user_ids.index(linesplit[0])
+		#then we find the index of the movie id in the movie_ids array
+		curr_movie = movie_ids.index(linesplit(',')[1])
+		
+		a[curr_user, curr_movie] = linesplit(',')[2]
+
+for x in range(8500):
+	if(a[3,x] != -1):
+		print("User_ID: ", user_ids[3], " -- Movie_ID: ", movie_ids[x], " -- Rating: ", a[3,x])
+		
+	
+	
+	
